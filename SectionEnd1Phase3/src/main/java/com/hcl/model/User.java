71f25210bdf713a6624b11c28fb24f3c9701b827
asimplecat <31932043+asimplecat@users.noreply.github.com>
2021-01-18 15:21:10 -0500

    Add files via upload

---
+++ SectionEnd1Phase3/src/main/java/com/hcl/model/User.java
@@ -0,0 +1,23 @@
+package com.hcl.model;
+
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "User")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+
+}
